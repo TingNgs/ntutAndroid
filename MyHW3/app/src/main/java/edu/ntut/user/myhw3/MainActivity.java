d35496f65668168c700f9f5e9e69159dae1d67d5
ngSgn <t105590049@ntut.org.tw>
2018-04-11 13:40:39 +0800

    upload HW4.1

--- MyHW3/app/src/main/java/edu/ntut/user/myhw3/MainActivity.java
+++ MyHW3/app/src/main/java/edu/ntut/user/myhw3/MainActivity.java
@@ -4,51 +4,76 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner mSpnSex;
-    private RadioGroup mRadGrp;
-    private RadioButton mRadBtnAgeRange1;
-    private RadioButton mRadBtnAgeRange2;
-    private RadioButton mRadBtnAgeRange3;
-    private TextView mTxtNumFamily;
-    private NumberPicker mNumPkrFamily;
+    private RadioButton mRadBtnSexMale;
+    private RadioButton mRadBtnSexFemale;
+    private Spinner mSpnAgeRange;
     private Button mBtnOK;
     private TextView mTxtSug;
-
-    private String selectedSex;
+    private TextView mTxtHob;
+    private CheckBox mChkBoxMusic;
+    private CheckBox mChkBoxSing;
+    private CheckBox mChkBoxDance;
+    private CheckBox mChkBoxTravel;
+    private CheckBox mChkBoxReading;
+    private CheckBox mChkBoxWriting;
+    private CheckBox mChkBoxClimbing;
+    private CheckBox mChkBoxSwim;
+    private CheckBox mChkBoxEating;
+    private CheckBox mChkBoxDrawing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSpnSex = (Spinner) findViewById(R.id.spnSex);
-        mRadGrp = (RadioGroup) findViewById(R.id.radGrpAge);
-        mRadBtnAgeRange1 = (RadioButton) findViewById(R.id.radBtnAgeRange1);
-        mRadBtnAgeRange2 = (RadioButton) findViewById(R.id.radBtnAgeRange2);
-        mRadBtnAgeRange3 = (RadioButton) findViewById(R.id.radBtnAgeRange3);
+        mRadBtnSexMale = (RadioButton) findViewById(R.id.radBtnSexMale);
+        mRadBtnSexFemale = (RadioButton) findViewById(R.id.radBtnSexFemale);
+        mSpnAgeRange = (Spinner) findViewById(R.id.spnAgeRange);
+        mBtnOK = (Button) findViewById(R.id.btnOK);
+        mTxtSug = (TextView) findViewById(R.id.txtSug);
+        mTxtHob = (TextView) findViewById(R.id.txtHobbies);
+        mChkBoxClimbing = (CheckBox) findViewById(R.id.chkBoxClimbing);
+        mChkBoxDance = (CheckBox) findViewById(R.id.chkBoxDance);
+        mChkBoxDrawing = (CheckBox) findViewById(R.id.chkBoxDrawing);
+        mChkBoxEating = (CheckBox) findViewById(R.id.chkBoxEating);
+        mChkBoxMusic = (CheckBox) findViewById(R.id.chkBoxMusic);
+        mChkBoxReading = (CheckBox) findViewById(R.id.chkBoxReading);
+        mChkBoxSing = (CheckBox) findViewById(R.id.chkBoxSing);
+        mChkBoxSwim = (CheckBox) findViewById(R.id.chkBoxSwim);
+        mChkBoxTravel = (CheckBox) findViewById(R.id.chkBoxTravel);
+        mChkBoxWriting = (CheckBox) findViewById(R.id.chkBoxWriting);
+
+        ArrayAdapter<CharSequence> maleList = ArrayAdapter.createFromResource(MainActivity.this, R.array.maleAgeRange, android.R.layout.simple_spinner_dropdown_item);
+        mSpnAgeRange.setAdapter(maleList);
+        mRadBtnSexMale.setOnClickListener(radBtnMaleOnLick);
+        mRadBtnSexFemale.setOnClickListener(radBtnFemaleOnLick);
+        mBtnOK.setOnClickListener(btnOKOnClick);
+        /*mRadGrp = (RadioGroup) findViewById(R.id.radGrpAge);
         mTxtNumFamily = (TextView) findViewById(R.id.txtNumFamily);
         mNumPkrFamily = (NumberPicker) findViewById(R.id.numPkrFamply);
         mNumPkrFamily.setMinValue(0);
         mNumPkrFamily.setMaxValue(20);
         mNumPkrFamily.setValue(3);
-        mBtnOK = (Button) findViewById(R.id.btnOK);
-        mTxtSug = (TextView) findViewById(R.id.txtSug);
 
         mSpnSex.setOnItemSelectedListener(spnOnItemSelect);
         mNumPkrFamily.setOnValueChangedListener(numPkrFamilyOnValueChange);
-        mBtnOK.setOnClickListener(btnOKOnClick);
+        */
     }
 
-    private AdapterView.OnItemSelectedListener spnOnItemSelect = new AdapterView.OnItemSelectedListener() {
+    /*private AdapterView.OnItemSelectedListener spnOnItemSelect = new AdapterView.OnItemSelectedListener() {
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
             selectedSex = parent.getSelectedItem().toString();
@@ -77,29 +102,46 @@ public class MainActivity extends AppCompatActivity {
             mTxtNumFamily.setText(String.valueOf(newVal));
         }
     };
-
+    */
     private View.OnClickListener btnOKOnClick = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
             MarriageSuggestion ms = new MarriageSuggestion();
+            String strSex;
+            String strHob = getString(R.string.hobbies);
+           if(mRadBtnSexMale.isChecked())
+                strSex = mRadBtnSexMale.toString().toLowerCase();
+            else
+                strSex = mRadBtnSexFemale.toString().toLowerCase();
+            int iAgeRange = mSpnAgeRange.getSelectedItemPosition()+1;
 
-            String strSex = mSpnSex.getSelectedItem().toString();
-            int iAgeRange = 0;
+            if(mChkBoxMusic.isChecked()) strHob+=getString(R.string.music)+" ";
+            if(mChkBoxSing.isChecked()) strHob+=getString(R.string.sing)+" ";
+            if(mChkBoxDance.isChecked()) strHob+=getString(R.string.dance)+" ";
+            if(mChkBoxTravel.isChecked()) strHob+=getString(R.string.travel)+" ";
+            if(mChkBoxReading.isChecked()) strHob+=getString(R.string.reading)+" ";
+            if(mChkBoxWriting.isChecked()) strHob+=getString(R.string.writing)+" ";
+            if(mChkBoxClimbing.isChecked()) strHob+=getString(R.string.climbing)+" ";
+            if(mChkBoxSwim.isChecked()) strHob+=getString(R.string.swim)+" ";
+            if(mChkBoxEating.isChecked()) strHob+=getString(R.string.eating)+" ";
+            if(mChkBoxDrawing.isChecked()) strHob+=getString(R.string.drawing)+" ";
+            mTxtSug.setText(ms.getSuggestion(strSex, iAgeRange));
+            mTxtHob.setText(strHob);
 
-                switch (mRadGrp.getCheckedRadioButtonId()) {
-                    case R.id.radBtnAgeRange1:
-                        iAgeRange = 1;
-                        break;
-                    case R.id.radBtnAgeRange2:
-                        iAgeRange = 2;
-                        break;
-                    case R.id.radBtnAgeRange3:
-                        iAgeRange = 3;
-                        break;
-                }
-
-            mTxtSug.setText(ms.getSuggestion(strSex, iAgeRange, mNumPkrFamily.getValue()));
+        }
+    };
+    private View.OnClickListener radBtnMaleOnLick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ArrayAdapter<CharSequence> maleList = ArrayAdapter.createFromResource(MainActivity.this, R.array.maleAgeRange, android.R.layout.simple_spinner_dropdown_item);
+            mSpnAgeRange.setAdapter(maleList);
+        }
+    };
+    private View.OnClickListener radBtnFemaleOnLick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            ArrayAdapter<CharSequence> femaleList = ArrayAdapter.createFromResource(MainActivity.this, R.array.femaleAgeRange, android.R.layout.simple_spinner_dropdown_item);
+            mSpnAgeRange.setAdapter(femaleList);
         }
     };
 }
